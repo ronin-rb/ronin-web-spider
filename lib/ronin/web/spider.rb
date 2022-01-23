49040f1c5101bd7c13053f51184b3d0fc3f9b4ff
Postmodern <postmodern.mod3@gmail.com>
2022-01-22 23:13:06 -0800

    Changed project name to ronin-web-spider.

--- lib/ronin/web/spider.rb
+++ lib/ronin/web/spider.rb
@@ -1,23 +1,22 @@
 #
-# Ronin Web - A Ruby library for Ronin that provides support for web
-# scraping and spidering functionality.
+# ronin-web-spider - A collection of common web spidering routines.
 #
 # Copyright (c) 2006-2021 Hal Brodigan (postmodern.mod3 at gmail.com)
 #
-# This file is part of ronin-web.
+# This file is part of ronin-web-spider.
 #
-# Ronin is free software: you can redistribute it and/or modify
+# ronin-web-spider is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Ronin is distributed in the hope that it will be useful,
+# ronin-web-spider is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Ronin.  If not, see <https://www.gnu.org/licenses/>.
+# along with ronin-web-spider.  If not, see <https://www.gnu.org/licenses/>.
 #
 
 require 'ronin/web/web'
