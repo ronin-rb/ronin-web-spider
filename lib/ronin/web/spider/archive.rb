333edff842be94df435b9d18852b574e3c9e4448
Postmodern <postmodern.mod3@gmail.com>
2022-08-28 17:33:59 -0700

    Fixed project name and description in `ronin/web/spider/archive`.

--- lib/ronin/web/spider/archive.rb
+++ lib/ronin/web/spider/archive.rb
@@ -1,20 +1,20 @@
 #
-# ronin-web-archive - 
+# ronin-web-spider - A collection of common web spidering routines.
 #
 # Copyright (c) 2022 Hal Brodigan (postmodern.mod3 at gmail.com)
 #
-# ronin-web-archive is free software: you can redistribute it and/or modify
+# ronin-web-spider is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published
 # by the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# ronin-web-archive is distributed in the hope that it will be useful,
+# ronin-web-spider is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with ronin-web-archive.  If not, see <https://www.gnu.org/licenses/>.
+# along with ronin-web-spider.  If not, see <https://www.gnu.org/licenses/>.
 #
 
 require 'fileutils'
